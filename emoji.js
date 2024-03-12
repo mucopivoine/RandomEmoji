10461c08020eba65564d5f5296a651ff043c13d0
mucopivoine <pivotmurerwa1@gmail.com>
2024-03-12 12:04:57 +0200

    Emojis stimulator using javascript

---
+++ emoji.js
@@ -0,0 +1,20 @@
+// const changeFace = document.getElementbyId('face');
+const emojis = [
+    "😂",
+    "😁",
+    "👍",
+    "👌",
+    "😉",
+    "😒",
+    "😎",
+    "🙄",
+    "😒",
+    "🤦‍♀️",
+];
+const face = document.querySelector("#face");
+const btn = document.getElementById("button");
+btn.addEventListener("click", () =>{
+  let round = Math.floor(Math.random() * emojis.length);
+  let result = emojis[round];
+  face.innerHTML = result;
+});
---
+++ index.js
@@ -0,0 +1,4 @@
+let b =[1,2,3,4,5];
+// random
+let a = Math.floor(Math.random() * emojis.length);
+console.log(a);
